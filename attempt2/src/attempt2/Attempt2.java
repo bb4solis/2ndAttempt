bf8689a43706acb5a73b8c638fefcbb81101ca7e
Brenda Solis <brendab4solis@gmail.com>
2021-04-13 19:51:24 -0700

    establishes: user credentials 			 handling of blank spaces

--- attempt2/src/attempt2/Attempt2.java
+++ attempt2/src/attempt2/Attempt2.java
@@ -1,19 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Java Code to launch the Second Term Project
  */
 package attempt2;
+import java.sql.*;
+import java.util.Scanner;
+import java.util.InputMismatchException;
 
 /**
  *
- * @author solis
+ * @author Chloe Bautista
+ * @author Kevin Corday
+ * @author Alex Chheng
+ * @author Brenda Solis
  */
 public class Attempt2 {
 
+    // Data base info
+    static String USER;
+    static String PASS;
+    static String DBNAME;
+    
     /**
-     * @param args the command line arguments
-     */
+ * Takes the input string and outputs "N/A" if the string is empty or null.
+ * @param input The string to be mapped.
+ * @return  Either the input string or "N/A" as appropriate.
+ */
+    public static String dispNull (String input) {
+        //because of short circuiting, if it's null, it never checks the length.
+        if (input == null || input.length() == 0)
+            return "N/A";
+        else
+            return input;
+    }
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("We did it BRODIES!");
